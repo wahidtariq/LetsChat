0db0011d91363dd89d87f9cdb68d4a45a727f679
wahidtariq <wahidzoonimar@gmail.com>
2022-03-12 15:24:38 +0530

    changed syntax

--- LetsChat/LoginPage.swift
+++ LetsChat/LoginPage.swift
@@ -15,62 +15,80 @@ struct LoginPage: View {
     var body: some View {
         NavigationView{
             ScrollView{
+                //MARK: - picker and image view textfields create account button
                 VStack(spacing: 12){
-                    Picker("", selection: $isLoginPage) {
-                        Text("Login")
-                            .tag(true)
-                        Text("Create Account.")
-                            .tag(false)
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
+                    picker
+                        .pickerStyle(SegmentedPickerStyle())
                     Spacer()
-
+                    
                     if !isLoginPage{
-                        Button{
-                            
-                        }label: {
-                            Image(systemName: "person.crop.circle.badge.plus")
-                                .resizable()
-                                .foregroundColor(.black.opacity(0.8))
-                                .frame(width: 100, height: 100)
-                                .padding()
-                        }
-                    }
-                    Spacer()
-
-                    Group{
-                        TextField("Email", text: $email)
-                            .keyboardType(.emailAddress)
-                            .textInputAutocapitalization(.never)
-                        SecureField("Password", text: $password)
-                    }
-                    .padding(12)
-                    .background(.white)
-                    Spacer()
-
-                    
-                    Button{
-                        handleAction()
-                    }label: {
-                        HStack{
-                            Spacer()
-                            Text(isLoginPage ? "Login" : "Create Account")
-                                .foregroundColor(.white)
-                                .padding(.vertical, 10)
-                                .font(.title3)
-                            Spacer()
-                        }
-                        .background(.blue.opacity(0.9))
-                        .cornerRadius(6)
+                        imageView
                     }
                     
+                    Spacer()
+                    textFields
+                        .padding(12)
+                        .background(.white)
+                    Spacer()
+                    createAccountButton
                 }
                 .padding()
             }
+            
             .navigationTitle(isLoginPage ? "Login" : "Create Account" )
             .background(Color.init(white: 0, opacity: 0.05).ignoresSafeArea())
         }
     }
+    
+    
+    
+    
+    var picker : some View {
+        Picker("", selection: $isLoginPage) {
+            Text("Login")
+                .tag(true)
+            Text("Create Account.")
+                .tag(false)
+        }
+    }
+    var imageView : some View{
+        Button{
+            
+        }label: {
+            Image(systemName: "person.crop.circle.badge.plus")
+                .resizable()
+                .foregroundColor(.black.opacity(0.8))
+                .frame(width: 100, height: 100)
+                .padding()
+        }
+    }
+    
+    var textFields: some View{
+        Group{
+            TextField("Email", text: $email)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+            SecureField("Password", text: $password)
+        }
+    }
+    
+    var createAccountButton: some View{
+        Button{
+            handleAction()
+        }label: {
+            HStack{
+                Spacer()
+                Text(isLoginPage ? "Login" : "Create Account")
+                    .foregroundColor(.white)
+                    .padding(.vertical, 10)
+                    .font(.title3)
+                Spacer()
+            }
+            .background(.blue.opacity(0.9))
+            .cornerRadius(6)
+        }
+    }
+    
     
     private func handleAction(){
         if isLoginPage{
